f1cb8d3afdc14022d0d4088f3aa815594af5d6eb
daro-decagon <gadibiao@decagonhq.com>
2023-08-21 09:57:49 +0100

    changes added

--- src/main/java/com/example/gapweekapi/controller/BookController.java
+++ src/main/java/com/example/gapweekapi/controller/BookController.java
@@ -21,8 +21,8 @@ public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping("get-a-book/{id}")
-    public ResponseEntity<Book> viewBook(@PathVariable Long id){
+    @GetMapping("get-a-todo/{id}")
+    public ResponseEntity<Book> todo(@PathVariable Long id){
 //        Address address = Address.builder()
 //                .id(1l)
 //                .name("Orchid")
@@ -51,9 +51,44 @@ public class BookController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         HttpEntity httpEntity = new HttpEntity(httpHeaders);
-        restTemplate.exchange("https://dummyjson.com/todos/1", HttpMethod.GET, httpEntity, Todo.class);
+        restTemplate.exchange("https://dummyjson.com/todos/"+id, HttpMethod.GET, httpEntity, Todo.class);
         return new ResponseEntity<>(bookService.viewBook(id), HttpStatus.OK);
     }
+
+//
+//    @GetMapping("get-a-book/{id}")
+//    public ResponseEntity<Book> viewBook(@PathVariable Long id){
+////        Address address = Address.builder()
+////                .id(1l)
+////                .name("Orchid")
+////                .build();
+////        Bank bank = Bank.builder()
+////                .id(1l)
+////                .name("Orchid Bank").build();
+////        Company company = Company.builder()
+////                .id(1l)
+////                .name("Orchid ltd")
+////                .address(address).build();
+////        Hair hair = Hair.builder()
+////                .color("Brown")
+////                .id(1l)
+////                .type("Wooly")
+////                .build();
+////        User user = User.builder()
+////                .company(company)
+////                .bank(bank)
+////                .hair(hair)
+////                .id(1l)
+////                .name("Orchid Daro")
+////                .build();
+////        System.out.println("This  is a sample: "+user);
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+//        restTemplate.exchange("https://dummyjson.com/todos/1", HttpMethod.GET, httpEntity, Todo.class);
+//        return new ResponseEntity<>(bookService.viewBook(id), HttpStatus.OK);
+//    }
 
     @PostMapping("save-a-book")
     public ResponseEntity<BookDTO> saveBook(@RequestBody BookDTO book){
