769d48f62c52c164e5913c79c0c0c73e23724025
daro-decagon <gadibiao@decagonhq.com>
2023-08-21 10:07:18 +0100

    changes added

--- src/main/java/com/example/gapweekapi/api/Todo.java
+++ src/main/java/com/example/gapweekapi/api/Todo.java
@@ -1,5 +1,12 @@
 package com.example.gapweekapi.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
     private Long id;
     private String todo;
